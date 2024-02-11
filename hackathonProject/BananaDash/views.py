c2a2dc6f95567418a340ec413f7c25eba79410a8
Lincoln Ledet <lincolnledet@gmail.com>
2024-02-10 23:03:53 -0500

    minimum viable product

--- hackathonProject/BananaDash/views.py
+++ hackathonProject/BananaDash/views.py
@@ -4,20 +4,16 @@ from requests_html import HTMLSession
 import requests
 
 
-
-
-
-
 def home(request):
     api_key = 'a588dff4066a4843903350b4de7c01b6'
     news_stories = fetch_news(api_key)
     print('test')
-    return render(request, 'BananaDash/index.html')
+    return render(request, 'BananaDash/index.html', {'news_stories': news_stories})
 # Create your views here.
 
 
 def fetch_news(api_key):
-    url = f"https://newsapi.org/v2/everything?q=banana&apiKey={api_key}"
+    url = f"https://newsapi.org/v2/everything?q=banana&sortBy=new&apiKey={api_key}"
     response = requests.get(url)
     if response.status_code == 200:
         print(response.json())
